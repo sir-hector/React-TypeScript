1a379d26f5b36e9491919d7b43986be864b2ba75
sir-hector <krauskarol7@gmail.com>
2022-09-26 21:29:33 +0200

    add props type - array

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
 import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
   const personName = {
     first: 'Bruce',
     last: 'Wayne'
   }
+
+  const nameList = [
+    {
+      first: "Bruce",
+      last: "Wayne",
+    },
+    {
+      first: "Wayne",
+      last: "Bruce",
+    },
+    {
+      first: "Princess",
+      last: "Diana",
+    },
+  ]
   return (
     <div className="App">
       <Greet name="Karol" messageCount={20} isLoggedIn={false} />
       <Person name={personName}/>
+      <PersonList names={nameList}/>
     </div>
   );
 }
