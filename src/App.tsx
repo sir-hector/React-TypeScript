5eee368727e54a48c213ecaa73dc712ed47baf6c
sir-hector <krauskarol7@gmail.com>
2022-09-26 21:23:14 +0200

    typing and object prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css';
-import { Greet } from './components/Greet';
+import "./App.css";
+import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
   return (
     <div className="App">
-        <Greet name="Karol" messageCount={20} isLoggedIn={false}/>
+      <Greet name="Karol" messageCount={20} isLoggedIn={false} />
+      <Person name={personName}/>
     </div>
   );
 }
