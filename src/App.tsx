8eae0ec610eedfb244852af5c6081e94b3e124cc
sir-hector <krauskarol7@gmail.com>
2022-09-27 16:07:48 +0200

    event Input props

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import "./App.css";
-import { Status } from "./components/Status";
-import { Heading } from "./components/Heading";
-import { Oscar } from "./components/Oscar";
-import { Greet } from "./components/Greet";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+
 
 function App() {
   return (
     <div className="App">
-      <Status status="error"/>
-      <Heading>Placeholder text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leaonardo Dicaprio!</Heading>
-      </Oscar>
-      <Greet name="Karol" isLoggedIn={true}></Greet>
+      <Button handleClick={(event, id)=> {
+        console.log('Button clicked', event, id)
+      }}/>
+      <Input value= '' handleChange={(event)=> {
+        console.log('Button clicked', event)
+      }}/>
     </div>
   );
 }
