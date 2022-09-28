9bf5c012b6a78783d178366c09d1b302514f2c00
sir-hector <krauskarol7@gmail.com>
2022-09-28 13:04:28 +0200

    type useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Login } from "./components/state/Login";
+import { User } from "./components/state/User";
 
 
 function App() {
   return (
     <div className="App">
       <Login></Login>
+      <User></User>
+
     </div>
   );
 }
