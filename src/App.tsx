ee8199bb677617066880523cd0184d289beec198
sir-hector <krauskarol7@gmail.com>
2022-09-30 15:07:37 +0200

    Polymorphic Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { CustomButton } from "./components/html/Button";
+import Text from "./components/polymorphic/Text";
 
 function App() {
   return (
     <div className="App">
       <CustomButton variant="primary" onClick={() => console.log('Clicked')} >Primary Button</CustomButton>
+      <Text as='h1' size="lg">Heading</Text>
+      <Text as='a' size="lg">Heading</Text>
     </div>
   );
 }
