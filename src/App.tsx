4645658c69d538ece035971afaa1bf17f85269d9
sir-hector <krauskarol7@gmail.com>
2022-09-30 13:37:27 +0200

    add generic props

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,24 @@ import { UserContextProvider } from "./components/context/UserContext";
 import { User } from "./components/context/User";
 import { Private } from "./components/auth/Private";
 import { Profile } from "./components/auth/Profile";
+import {List} from './components/generics/List'
 
 
 function App() {
   return (
     <div className="App">
       <Private isLoggedIn={true} component={Profile}></Private>
+      {/* <List items={['Batman', 'Superman', 'Wonder Woman']} onClick={(item) => console.log(item)}></List>
 
+      <List items={[1,2,3]} onClick={(item) => console.log(item)}></List> */}
+
+      <List items={[
+        {
+          id: 1,
+          first: 'Bruce',
+          last: 'Wayne'
+        }
+      ]} onClick={(item) => console.log(item)}></List>
     </div>
   );
 }
---
+++ src/components/generics/List.tsx
@@ -0,0 +1,19 @@
+type listProps<T> = {
+    items: T[]
+    onClick: (value: T) => void
+}
+
+export const List = <T extends { id: number}>({items, onClick}: listProps<T>) => {
+    return (
+        <div>
+            <h2>List of items</h2>
+            {items.map((item,index) => {
+                return (
+                    <div key={index} onClick={()=> onClick(item)}>
+                        {item.id}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
